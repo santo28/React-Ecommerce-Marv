ecbfd01c274e14e19a1d0b48c06f7bc4ed76f2e5
santo28 <santos17004@byui.edu>
2023-07-05 00:56:40 -0600

    added data

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+        <div>
+            <h1>Marvin's Ecommerce Project</h1>
+        </div>
+        <div className='products'>
+            <h1>Marvin's Ecommerce Project</h1>
+        </div>
+
+    </div>
+  )
+}
